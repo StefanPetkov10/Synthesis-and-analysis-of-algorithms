4dd997866e19e5871f1604a58fa6f71d9e2e845c
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-01-22 14:07:20 +0200

    Add bubble sort

---
+++ Sorts assignments/Bubble Sort/BubbleSortExample.cs
@@ -0,0 +1,40 @@
+﻿namespace Bubble_Sort
+{
+    class BubbleSortExample
+    {
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 64, 34, 25, 12, 22, 11, 90 };
+
+            Console.WriteLine("Before sort:");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            BubbleSort(numbers);
+
+            Console.WriteLine("\nAfter sort:");
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
