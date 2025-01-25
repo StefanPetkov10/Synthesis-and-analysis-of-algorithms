e5f42cc32c94aeeed731f8fea9e4df6d13529dd3
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-01-25 15:10:58 +0200

    Add an algorithmic task

---
+++ Аssignments from previous years/task_4/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace task_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 60, 48, 120, 36 };
+
+            // Finding the GCD manually
+            int gcd = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                gcd = FindGCD(gcd, numbers[i]);
+            }
+
+            // Sorting the array manually (Bubble Sort)
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($"The GCD of the array is: {gcd}");
+            Console.WriteLine("Sorted array: " + string.Join(", ", numbers));
+        }
+
+        static int FindGCD(int a, int b)
+        {
+            //алгоритъм на Евклид.
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
