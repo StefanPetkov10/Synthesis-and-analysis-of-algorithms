e1d3b48a4cf812e930bb2d657a8c2f3550c68178
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-01-25 15:10:51 +0200

    Add an algorithmic task

---
+++ Аssignments from previous years/task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 50, 102, 150, 9999, 10000, 200, 3050, 75 };
+            int count = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number > 100 && number < 10000 && number % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"The count of even numbers greater than 100 and less than 10,000 is: {count}");
+        }
+    }
+}
