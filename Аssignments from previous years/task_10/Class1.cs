db32b253600e01dfcbe661944b7c5cc000a5833b
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-01-27 20:33:19 +0200

    Add an algorithmic task

---
+++ Аssignments from previous years/task_10/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace task_10
+{
+    class Class1
+    {
+        public void SetExample()
+        {
+            int[] A = { 1, 2, 3, 4, 5 };
+            int[] B = { 4, 5, 6, 7, 8 };
+            int[] C = { 2, 4, 8, 10, 12 };
+
+            HashSet<int> setA = new HashSet<int>(A);
+            HashSet<int> setB = new HashSet<int>(B);
+            HashSet<int> setC = new HashSet<int>(C);
+
+            setA.IntersectWith(setB);
+            setA.IntersectWith(setC);
+
+            List<int> common = new List<int>(setA);
+
+            foreach (int item in common)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ Аssignments from previous years/task_10/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace task_10
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 2, 3, 6, 7 };
+            int[] array3 = { 1, 3, 4, 8 };
+
+            var commonValues = FindCommonValues(array1, array2, array3);
+
+            Console.WriteLine("United equal values:");
+            foreach (var value in commonValues)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("Set Example:");
+            var class1Instance = new Class1();
+            class1Instance.SetExample();
+        }
+
+        public static List<int> FindCommonValues(int[] array1, int[] array2, int[] array3)
+        {
+            var valueCount = new Dictionary<int, int>();
+            var commonValues = new List<int>();
+
+            foreach (var value in array1)
+            {
+                if (!valueCount.ContainsKey(value))
+                    valueCount[value] = 1;
+            }
+
+            foreach (var value in array2)
+            {
+                if (valueCount.ContainsKey(value) && valueCount[value] == 1)
+                    valueCount[value] = 2;
+            }
+
+            foreach (var value in array3)
+            {
+                if (valueCount.ContainsKey(value) && valueCount[value] == 2)
+                {
+                    commonValues.Add(value);
+                }
+            }
+
+            return commonValues;
+        }
+    }
+}
+
+
